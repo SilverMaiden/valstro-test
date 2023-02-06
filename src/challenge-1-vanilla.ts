4e2a2002319b0867b6fbb0913499f8c458b0070f
Humaira Summit <h.specials@gmail.com>
2023-02-06 14:00:58 -0800

    Added some error handling for vanilla js test

--- src/challenge-1-vanilla.ts
+++ src/challenge-1-vanilla.ts
@@ -1,134 +1,173 @@
 // Note: The HTML for this challenge can be found in index.html
 // Note: this function is run inside of src/main.tsx
 
-type Character = {
+// Using interface over type, so as to have the option to extend it.
+export interface Character {
   name: string;
   height: string;
   mass: string;
+}
+
+export const POWER_CELL_INDEX = 3;
+export const DEFAULT_MULTIPLIER = "10";
+export const DEFAULT_FILTER_INPUT = "";
+
+//-------- Function TO PARSE THE DATA WE NEED --------//
+export const parseCharacterData = (
+  data: { results: any[] },
+  characterTableInfo: Character[]
+) => {
+  data.results.forEach((character) => {
+    const { name, height, mass } = character;
+    characterTableInfo.push({
+      name,
+      height,
+      mass,
+    });
+  });
 };
 
-export const runVanillaApp = () => {
-  //-------- Function TO PARSE THE DATA WE NEED --------//
-  const parseCharacterData = (
-    data: { results: any[] },
-    characterTableInfo: Character[]
-  ) => {
-    data.results.forEach((character) => {
-      const { name, height, mass } = character;
-      characterTableInfo.push({
-        name,
-        height,
-        mass,
-      });
-    });
-  };
-
-  const calculatePower = (height: string, mass: string) => {
-    const intHeight = parseInt(height);
-    const intMass = parseInt(mass);
-    const power =
-      intHeight && intMass
-        ? (intHeight * intMass * parseInt(multiplierInput.value)).toString()
-        : "-";
-    return power;
-  };
-
-  //-------- FUNCTION FOR FETCHING CHARACTERS --------//
-  const fetchCharacters = async () => {
-    const characterTableInfo: Character[] = [];
-    let nextPage = "https://swapi.dev/api/people/";
-
+export const calculatePower: (
+  height: string,
+  mass: string,
+  multiplier: string
+) => string = (height, mass, multiplier) => {
+  const intHeight = parseInt(height);
+  const intMass = parseInt(mass);
+  const intMultiplier = parseInt(multiplier);
+  const power =
+    intHeight && intMass && intMultiplier
+      ? (intHeight * intMass * intMultiplier).toString()
+      : "-";
+  return power;
+};
+const throwError = async () => {
+  throw new Error(`Something went wrong with your api call.`);
+};
+//-------- FUNCTION FOR FETCHING CHARACTERS --------//
+export const fetchCharacters = async () => {
+  let tableData: string | null = localStorage.getItem("tableData");
+  const characterTableInfo: Character[] = [];
+  let nextPage = "https://swapi.dev/api/people/";
+  if (!tableData) {
     while (nextPage) {
       const response = await fetch(nextPage);
       const data = await response.json();
       parseCharacterData(data, characterTableInfo);
       nextPage = data.next;
     }
-    localStorage.setItem("tableData", JSON.stringify(characterTableInfo));
-  };
+    let returnVal = JSON.stringify(characterTableInfo);
+    localStorage.setItem("tableData", returnVal);
+    return returnVal;
+  } else {
+    return tableData;
+  }
+};
 
+export const runVanillaApp: () => void = () => {
   const multiplierInput = document.getElementById(
     "multiplier"
   ) as HTMLInputElement;
   const filterInput = document.getElementById("filter") as HTMLInputElement;
 
   // -------- CACHE DATA -------- //
-  const cacheData: string | null = localStorage.getItem("tableData");
+  let cacheData: string | null;
+  let characterTableInfo: Character[] = [];
   const tableBody = document.querySelector("#tbody");
-  if (!cacheData) {
-    fetchCharacters();
-  } else {
-    const characterTableInfo = JSON.parse(cacheData);
+  //localStorage.clear();
+  let loader = document.getElementById("loader");
 
-    //-------- MULTIPLIER CHANGE HANDLER --------//
-    multiplierInput.addEventListener("input", () => {
-      characterTableInfo.forEach((character: Character, index: number) => {
-        const power = calculatePower(character.height, character.mass);
-        (tableBody?.children[index].children[3] as HTMLElement).innerText =
-          power;
-      });
+  fetchCharacters()
+    .then((response) => {
+      cacheData = response;
+      loadDataIntoTable();
+      if (loader) {
+        loader.style.display = "none";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throwError();
     });
 
-    //-------- Filter the table's list by the name field --------//
-    filterInput.addEventListener("input", () => {
-      const filterValue = filterInput.value.toLowerCase();
+  //-------- MULTIPLIER CHANGE HANDLER --------//
+  multiplierInput.addEventListener("input", () => {
+    characterTableInfo.forEach((character: Character, index: number) => {
+      const power = calculatePower(
+        character.height,
+        character.mass,
+        multiplierInput.value
+      );
+      (
+        tableBody?.children[index].children[POWER_CELL_INDEX] as HTMLElement
+      ).innerText = power;
+    });
+  });
+
+  //-------- Filter the table's list by the name field --------//
+  filterInput.addEventListener("input", () => {
+    const filterValue = filterInput.value.toLowerCase();
+    for (let i = 0; i < characterTableInfo.length; i++) {
+      const row = tableBody?.children[i] as HTMLElement;
+      if (characterTableInfo[i].name.toLowerCase().includes(filterValue)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    }
+  });
+
+  //--------- KEYDOWN EVENT LISTENER --------//
+  document.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      filterInput.value = DEFAULT_FILTER_INPUT;
+      multiplierInput.value = DEFAULT_MULTIPLIER;
       for (let i = 0; i < characterTableInfo.length; i++) {
         const row = tableBody?.children[i] as HTMLElement;
-        if (characterTableInfo[i].name.toLowerCase().includes(filterValue)) {
-          row.style.display = "";
-        } else {
-          row.style.display = "none";
-        }
+        row.style.display = "";
+        const power = calculatePower(
+          characterTableInfo[i].height,
+          characterTableInfo[i].mass,
+          multiplierInput.value
+        );
+        (row.children[POWER_CELL_INDEX] as HTMLElement).innerText = power;
       }
-    });
+    }
+  });
 
-    //--------- KEYDOWN EVENT LISTENER --------//
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
-        filterInput.value = "";
-        multiplierInput.value = "10";
-        for (let i = 0; i < characterTableInfo.length; i++) {
-          const row = tableBody?.children[i] as HTMLElement;
-          row.style.display = "";
-          const power = calculatePower(
-            characterTableInfo[i].height,
-            characterTableInfo[i].mass
-          );
-          (row.children[3] as HTMLElement).innerText = power;
-        }
-      }
-    });
+  //-------- LOAD DATA INTO TABLE --------//
+  const loadDataIntoTable = async () => {
+    if (tableBody && cacheData) {
+      characterTableInfo = JSON.parse(cacheData);
+      tableBody.innerHTML = "";
 
-    //-------- LOAD DATA INTO TABLE --------//
-    const loadDataIntoTable = async () => {
-      if (tableBody && cacheData) {
-        const characterTableInfo = JSON.parse(cacheData);
-        tableBody.innerHTML = "";
+      characterTableInfo.forEach((characterRow: Character) => {
+        const { name, height, mass } = characterRow;
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const heightCell = document.createElement("td");
+        const massCell = document.createElement("td");
+        const powerCell = document.createElement("td");
 
-        characterTableInfo.forEach((characterRow: Character) => {
-          const { name, height, mass } = characterRow;
-          const row = document.createElement("tr");
-          const nameCell = document.createElement("td");
-          const heightCell = document.createElement("td");
-          const massCell = document.createElement("td");
-          const powerCell = document.createElement("td");
+        nameCell.innerText = name;
+        heightCell.innerText = height;
+        massCell.innerText = mass;
+        powerCell.innerText = calculatePower(
+          height,
+          mass,
+          multiplierInput.value
+        );
 
-          nameCell.innerText = name;
-          heightCell.innerText = height;
-          massCell.innerText = mass;
-          powerCell.innerText = calculatePower(height, mass);
+        row.appendChild(nameCell);
+        row.appendChild(heightCell);
+        row.appendChild(massCell);
+        row.appendChild(powerCell);
 
-          row.appendChild(nameCell);
-          row.appendChild(heightCell);
-          row.appendChild(massCell);
-          row.appendChild(powerCell);
+        tableBody.appendChild(row);
+      });
+    }
+  };
 
-          tableBody.appendChild(row);
-        });
-      }
-    };
-
-    //-------- Run the loadDataIntoTable function --------//
-    loadDataIntoTable();
-  }
-}
+  //-------- Run the loadDataIntoTable function --------//
+  loadDataIntoTable();
+};
--- src/challenge-2-ReactFunctionalComp.tsx
+++ src/challenge-2-ReactFunctionalComp.tsx
@@ -1,17 +1,81 @@
-function FunctionalComp() {
+import { useCallback, useEffect, useState } from "react";
+import {
+  calculatePower,
+  Character,
+  DEFAULT_FILTER_INPUT,
+  DEFAULT_MULTIPLIER,
+  fetchCharacters,
+} from "./challenge-1-vanilla";
+
+const FunctionalComp = () => {
+  let [cacheData, setCacheData] = useState<string | null>(
+    localStorage.getItem("tableData")
+  );
+  const [multiplier, setMultiplier] = useState<string>(DEFAULT_MULTIPLIER);
+  const [loading, setLoading] = useState<Boolean>(false);
+  const [error, setError] = useState<Error>();
+  const [filterInput, setFilterInput] = useState<string>("");
+
+  const escFunction = useCallback((event: KeyboardEvent) => {
+    if (event.key === "Escape") {
+      setMultiplier(DEFAULT_MULTIPLIER);
+      setFilterInput(DEFAULT_FILTER_INPUT);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, [escFunction]);
+
+  useEffect(() => {
+    console.log(cacheData);
+    if (!cacheData) {
+      setLoading(true);
+      fetchCharacters()
+        .then((response) => {
+          setLoading(false);
+          setCacheData(response);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    }
+  }, [localStorage.getItem("tableData")]);
+
+  const characterTableInfo: Character[] = cacheData
+    ? JSON.parse(cacheData).filter((character: Character) => {
+        return character.name.toLowerCase().includes(filterInput);
+      })
+    : [];
+
   return (
     <div id="functional-comp">
       <h2>React Functional Component</h2>
-      Filter: <input placeholder="Filter by name" /> Multiplier:{" "}
+      Filter:
+      <input
+        placeholder="Filter by name"
+        value={filterInput}
+        onChange={(event) => {
+          setFilterInput(event.target.value);
+        }}
+      />{" "}
+      Multiplier:{" "}
       <input
         placeholder="Multiplier"
         type="number"
         min="1"
         max="20"
-        defaultValue="10"
+        value={multiplier}
+        onChange={(event) => {
+          setMultiplier(event.target.value);
+        }}
       />{" "}
       Press "Escape" to reset fields
-      <div className="loader">Loading...</div>
+      {loading && <div className="loader">Loading...</div>}
       <table width="100%">
         <thead>
           <tr>
@@ -22,17 +86,22 @@ function FunctionalComp() {
           </tr>
         </thead>
         <tbody>
-          {/* This is just an example, please remove */}
-          <tr>
-            <td>Example Character (please remove)</td>
-            <td>100</td>
-            <td>50</td>
-            <td>50000 (10 * 100 * 50)</td>
-          </tr>
+          {characterTableInfo.map((character: Character) => {
+            const { name, height, mass } = character;
+            let power = calculatePower(height, mass, multiplier);
+            return (
+              <tr key={`${name}-${power}`}>
+                <td>{name}</td>
+                <td>{height}</td>
+                <td>{mass}</td>
+                <td>{power}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default FunctionalComp;
